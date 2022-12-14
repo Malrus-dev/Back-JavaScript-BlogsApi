9e446640476881f8a2611d47f11cf224515e9619
Malrus-dev <maelsobfer@gmail.com>
2022-12-14 11:11:27 -0300

    migrations feitas

---
+++ src/migrations/20221214134708-posts_categories.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', {
+      post_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      category_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+  }
+};
