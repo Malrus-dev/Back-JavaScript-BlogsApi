3b262744e75db97ef196d7d5798d051692cc9936
Malrus-dev <maelsobfer@gmail.com>
2022-12-14 11:32:17 -0300

    UserModel feito

---
+++ src/models/user.model.js
@@ -0,0 +1,22 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true
+  });
+
+  return User;
+};
+
+module.exports = UserModel;
